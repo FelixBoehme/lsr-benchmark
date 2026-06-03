da6f357f750e01782e1d49eb32d417e680c166cd
Maik Fröbe <kibi9872@webis3.medien.uni-weimar.de>
2026-06-03 16:17:25 +0200

    mf

--- step-02-embedding-approaches/sentence-transformers/sentence_transformers_embeddings.py
+++ step-02-embedding-approaches/sentence-transformers/sentence_transformers_embeddings.py
@@ -8,6 +8,8 @@ import lsr_benchmark
 from lsr_benchmark.click import option_lsr_dataset
 import ir_datasets
 
+TRUNCATE_LENGTH = 512
+
 
 def convert_embeddings_dense(embeddings: np.ndarray):
     n_docs, n_dims = embeddings.shape
@@ -18,8 +20,20 @@ def convert_embeddings_dense(embeddings: np.ndarray):
     return values, col_idcs, row_indices
 
 
+def truncate_texts(texts):
+    ret = []
+    for t in texts:
+        # an guess on the upper bound for transformer tokens is 10 characters per token.
+        ret.append(t[:(10*TRUNCATE_LENGTH)])
+    return ret
+
+
 def embedd_text_with_model(model, texts, ids, output):
     output.parent.mkdir(parents=True, exist_ok=True)
+
+    # not all models use the passed module.tokenizer.model_max_length
+    texts = truncate_texts(texts)
+
     with tracking(export_file_path=str(output).replace("-embeddings.npz", "-ir-metadata.yml")):
         embeddings = model.encode(texts, show_progress_bar=True, normalize_embeddings=True)
     data, indices, indptr = convert_embeddings_dense(embeddings)
@@ -34,7 +48,7 @@ def embedd_text_with_model(model, texts, ids, output):
 def main(dataset: str, model: str, batch_size: int, output: Path):
     lsr_benchmark.register_to_ir_datasets(dataset)
     module = SentenceTransformer(model)
-    module.tokenizer.model_max_length = 512
+    module.tokenizer.model_max_length = TRUNCATE_LENGTH
     register_metadata({"actor": {"team": "sentence-transformers"}, "tag": model.replace('/', '-')})
 
     dataset_id = f"lsr-benchmark/{dataset}"
